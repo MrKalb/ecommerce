6a7e9da2f4f25e2cf60100a682cae81ec17f2252
Igor Kalb <ikalb9@gmail.com>
2017-06-08 11:27:24 -0300

    Merge origin/master

--- Ecommerce/src/main/java/ecommerce/Dao/CarrinhoDao.java
+++ Ecommerce/src/main/java/ecommerce/Dao/CarrinhoDao.java
@@ -18,15 +18,15 @@ import javax.persistence.TypedQuery;
  */
 public class CarrinhoDao extends AbstractDao<Carrinho> {
 
-    private EntityManager em;
+    private EntityManager em = getEntityManager();
     
     public CarrinhoDao(EntityManager em) {
         super(em, Carrinho.class);
     }
 
     public Carrinho getCarrinho(Integer a) {
-        TypedQuery<Carrinho> p1 = em.createQuery("select c from carrinho c join cliente cl on c.idcliente = cl.id"
-                + "where c.idcliente = :id and flgvenda <> 'S'",Carrinho.class);
+        TypedQuery<Carrinho> p1 = em.createQuery("select c from Carrinho c join Cliente cl on c.cliente.id = cl.id"
+                + "where c.cliente.id = :id and flgvenda <> 'S'",Carrinho.class);
          return p1.setParameter("id", a).getSingleResult();
     }
 
